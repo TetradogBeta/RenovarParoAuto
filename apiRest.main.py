46b6041c1ca14dfe5dd843e2276720696c1a83ff
Gabriel Cortés Garcia <tetradog@gmail.com>
2021-09-07 05:09:05 +0200

    ahora genera automaticamente las claves del ssl aunque de momento no acaba de ir...

--- apiRest.main.py
+++ apiRest.main.py
@@ -7,7 +7,10 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 import json
 
+from OpenSSL import crypto, SSL
 
+certificate="certificate.pem";
+private="private.pem";
 port=5002;
 app = Flask(__name__);
 cors = CORS(app, resources={r"/*": {"origins": "https://tetradogbeta.github.io/RenovarParoAuto/*"}});#
@@ -63,5 +66,45 @@ def IsServerWakeUp():
     return isWakeUp;
 
 
+
+def GenSSL(
+    emailAddress="gabriel.cat.developer@gmail.com",
+    commonName="GabrielCatDeveloper",
+    countryName="es",
+    localityName="Girona",
+    stateOrProvinceName="Girona",
+    organizationName="GabrielCatDeveloper",
+    organizationUnitName="GabrielCatDeveloper",
+    serialNumber=4546485,
+    validityStartInSeconds=0,
+    validityEndInSeconds=10*365*24*60*60,
+    keyFile = "private.key",
+    certFile="selfsigned.crt"):
+    #can look at generated file using openssl:
+    #openssl x509 -inform pem -in selfsigned.crt -noout -text
+    # create a key pair
+    k = crypto.PKey()
+    k.generate_key(crypto.TYPE_RSA, 4096)
+    # create a self-signed cert
+    cert = crypto.X509()
+    cert.get_subject().C = countryName
+    cert.get_subject().ST = stateOrProvinceName
+    cert.get_subject().L = localityName
+    cert.get_subject().O = organizationName
+    cert.get_subject().OU = organizationUnitName
+    cert.get_subject().CN = commonName
+    cert.get_subject().emailAddress = emailAddress
+    cert.set_serial_number(serialNumber)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(validityEndInSeconds)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(k)
+    cert.sign(k, 'sha512')
+    with open(certFile, "wt") as f:
+        f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8"))
+    with open(keyFile, "wt") as f:
+        f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8"))
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=str(port),debug=False,ssl_context=('cert.pem', 'key.pem'));
+    GenSSL(keyFile=private,certFile=certificate);
+    app.run(host="0.0.0.0",port=str(port),debug=False,ssl_context=(certificate, private));
